1f14df6179db66c37cc80cb4c488d9825e283c0b
Diana_Akinshina <diana.10_@mail.ru>
2022-09-07 13:07:49 +0300

    BMI

--- BMI_HW_2 copy.py
+++ BMI_HW_2 copy.py
@@ -6,6 +6,6 @@ user_BMI = round(user_weight/(user_hight/100)**2,2)
 print(f'''
       Индекс массы вашего тела составляет: {user_BMI}
                      
-            {'10'+'='*int(user_BMI)+'|'+'='*(45-int(user_BMI))+"45"}'''
+      {'10'+'='*int(user_BMI)+'|'+'='*(45-int(user_BMI))+"45"}'''
      )
 
