ec35bc800d595d99935e5aada96f6259b4ebe266
Diana_Akinshina <diana.10_@mail.ru>
2022-10-05 22:49:08 +0300

    sec key

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from . import local_settings
 from pathlib import Path
 
-from proj.local_settings import SECRET_KEY_
+from proj.local_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
