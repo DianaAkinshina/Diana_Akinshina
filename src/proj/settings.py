aabbd5603c490bac3e01a43a6bc1e384d053ce02
Diana_Akinshina <diana.10_@mail.ru>
2022-10-05 21:19:09 +0300

    allowed hosts

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SECRET_KEY_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dianaakinshina.pythonanywhere.com']
 
 
 # Application definition
